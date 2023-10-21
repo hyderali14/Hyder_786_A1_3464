a0c436f89162564906f068325ebd801c6490bb4f
Hyder Ali <hyderrali123@gmail.com>
2023-10-21 19:10:44 -0400

    A1 first

--- Hyder_786_A1_3464/src/Main.java
+++ Hyder_786_A1_3464/src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+import java.lang.Math;
+
+class Point {
+    double x, y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 }
+
+class Triangle{
+    Point p1, p2, p3;
+
+    Triangle(Point p1, Point p2, Point p3){
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+}
+
